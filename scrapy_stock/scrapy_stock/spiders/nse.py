584325e39835182a9996176c1c719bacd88707ba
SagarSapre <132207478+SagarSapre@users.noreply.github.com>
2024-09-23 12:43:28 +0530

    scrapy+api

--- scrapy_stock/scrapy_stock/spiders/nse.py
+++ scrapy_stock/scrapy_stock/spiders/nse.py
@@ -1,10 +1,28 @@
 import scrapy
-
+from scrapy_stock.items import ScrapyStockItem
+import datetime
 
 class NseSpider(scrapy.Spider):
     name = "nse"
     allowed_domains = ["www.nseindia.com"]
     start_urls = ["https://www.nseindia.com"]
 
-    def parse(self, response):
+    def requests():
         pass
+
+    
+    def parse(self, response):
+        date=response.css('span.nifty50-tradedate')
+        scrapeddate=date.css('.nifty50-tradedate::text').get()
+        scrapeddate=scrapeddate.split(' ')[0]
+        scrapeddateitem=ScrapyStockItem()
+        todaydate=datetime.datetime.today().strftime('%d-%b-%Y')
+        #randomdate=datetime.datetime(2024, 9, 17)
+        print (f"today is __{todaydate}__")
+        scrapeddateitem['ScrapedDate'] = scrapeddate
+        if todaydate == scrapeddate:
+            Marketopen = True
+        else:
+            Marketopen = False
+        print (f"today is Marketopen __{Marketopen}__")
+        yield scrapeddateitem
--- scrapy_stock/scrapy_stock/spiders/stockspider.py
+++ scrapy_stock/scrapy_stock/spiders/stockspider.py
@@ -4,14 +4,12 @@ import scrapy
 class StockspiderSpider(scrapy.Spider):
     name = "stockspider"
     allowed_domains = ["www.nseindia.com"]
-    start_urls = ["https://www.nseindia.com"]
+    start_urls = ["c"]
 
     def start_requests(self):
         url = 'https://www.nseindia.com'
         yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
-        
-        ScrapedDate=response.css()
 
         pass
